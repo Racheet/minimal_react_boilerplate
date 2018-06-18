01a034769ac008ecdc35d3727615d66d7d69e768
Racheet Dave <racheet@gmail.com>
2018-06-18 16:04:57 +0000

    minor tweaks to allow for jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { Component, createElement } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import { PropTypes } from 'prop-types'
 
 
-render(createElement("p",null,"Webpack Loaded OK"),document.getElementById('react-container'))
+//render(createElement("p",null,"Webpack Loaded OK"),document.getElementById('react-container'))
+//
+render(<p>React loaded ok</p>,document.getElementById('react-container'))

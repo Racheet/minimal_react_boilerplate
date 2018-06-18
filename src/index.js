a652ff8c87705d35fb46e17e985f92653d3957f3
Racheet Dave <racheet@gmail.com>
2018-06-18 16:07:05 +0000

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import { render } from 'react-dom'
 import { PropTypes } from 'prop-types'
 
 
-//render(createElement("p",null,"Webpack Loaded OK"),document.getElementById('react-container'))
-//
 render(<p>React loaded ok</p>,document.getElementById('react-container'))
